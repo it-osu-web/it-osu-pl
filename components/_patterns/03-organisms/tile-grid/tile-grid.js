e27ea31e5caac4c5496c47fc2273ff444d961fdb
M Miller <miller.2676@gmail.com>
2020-02-27 18:41:18 -0800

    finish tile grid pattern

---
+++ components/_patterns/03-organisms/tile-grid/tile-grid.js
@@ -0,0 +1,22 @@
+(function($) {
+
+  'use strict';
+
+  // // Find tile grids with specified items per row.
+  // var tileGridRow = $('.row-items');
+
+  // tileGridRow.each(function() {
+  //   var itemsPerRow = $(this).attr('row-items');
+  //   var tile = $(this).find('a.tile');
+
+
+
+  //     $( "<p>Test</p>" ).insertAfter( ".item-3" );
+
+
+  //   console.log(itemsPerRow);
+  // })
+
+
+
+})(jQuery);
