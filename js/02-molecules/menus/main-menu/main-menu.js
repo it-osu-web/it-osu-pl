fb17b2e1e7a56e45865f6cf232632dd46e9f09c8
mmiller <miller.2676@osu.edu>
2020-07-06 12:08:02 -0700

    Publish gh-pages: auto-generated commit via gulp.

--- js/02-molecules/menus/main-menu/main-menu.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function(event) {
-  var options = {
-    ariaLabel: 'Main Navigation',
-    breakpointMinWidth: 720,
-    mode: 'dualAction',
-    responsiveSubmenuToggles: true,
-  };
-  var mainMenu = new a11yNavbar('main-nav', options);
-});
