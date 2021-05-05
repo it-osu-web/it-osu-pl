c58fc70d888208b1a358bf566daa23e7aec87d13
mmiller <miller.2676@osu.edu>
2021-05-05 14:33:21 -0700

    fix tile grid width issue

--- components/js/patterns/02-molecules/menus/main-menu/main-menu.js
+++ components/js/patterns/02-molecules/menus/main-menu/main-menu.js
@@ -1,9 +1,32 @@
-window.addEventListener('load', function(event) {
+var minWidth = 640;
+
+// Applies a11y navbar script.
+window.addEventListener('load', function (event) {
   var options = {
     ariaLabel: 'Main Navigation',
-    breakpointMinWidth: 640,
+    breakpointMinWidth: minWidth,
     mode: 'dualAction',
     responsiveSubmenuToggles: true,
   };
   var mainMenu = new a11yNavbar('main-nav', options);
 });
+
+// Removes grid container for mobile menu.
+var windowWidth = $(window).width();
+
+(function ($) {
+
+  var detectMobile = function () {
+    if (windowWidth < minWidth) {
+      $('.layout__inner--primary-menu').removeClass('grid-container');
+    }
+  }
+
+  // Call function on page load and resize.
+  // detectMobile();
+  
+  // $(window).on('resize', function() {
+  //   detectMobile();
+  // });
+
+})(jQuery);
